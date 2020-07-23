c366ff79b049f96ec505ca882625abd3fa906119
vaibhavpatelai <68038517+vaibhavpatelai@users.noreply.github.com>
2020-07-23 15:55:30 +0530

    Add files via upload

---
+++ Day11/Correlation-Assignment.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+dataset = pd.read_csv("/Users/VAIBHAV/Vaibhav/Python/LetUpgrade/LetsUpgradeAssignment/Day11/general_data.csv")
+dataset.head()
